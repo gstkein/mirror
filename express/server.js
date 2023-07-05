29cbe21dae8da8eb471f520cabaa06cf1eb73ccc
Guillermo Steren <gstkein@gmail.com>
2023-07-05 16:39:08 -0300

    changes

---
+++ express/server.js
@@ -0,0 +1,37 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+
+
+app.get("/:parametro", (req, res) => {
+  const parametro = req.params.parametro;
+  const response = { respuesta: parametro };
+  res.json(response);
+});
+
+app.get("/", (req, res) => {
+  const parametro = req.query.parametro;
+  const response = { parametro: parametro };
+  res.json(response);
+});
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
